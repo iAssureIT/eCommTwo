cabde00f426e860a112048357fab083dd4da4255
KarunaKhandale28 <gaurikhandale97@gmail.com>
2020-05-19 18:35:31 +0530

    Karuna Added changes in Unumandai Website

--- WebApp/onlineStore/src/coreEngine/lib/Routes.js
+++ WebApp/onlineStore/src/coreEngine/lib/Routes.js
@@ -1,21 +1,20 @@
 import React,{Component}                          from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import $ from 'jquery';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/js/modal.js';
 import 'bootstrap/js/collapse.js';
 import 'bootstrap/js/tab.js';
 import 'font-awesome/css/font-awesome.min.css';
+// import $ from 'jquery';
 
 import '../../App.css';
 //-----------Main Menu Rout.................//
 import Header                   from '../common/Header/Header.js';
-// import Header_Unimandai         from '../common/Header/Header_Unimandai.js';
+import Header_Unimandai         from '../common/Header/Header_Unimandai.js';
 import Footer                   from '../common/Footer/Footer.js'; 
-// import Footer_Unimandai                   from '../common/Footer/Footer_Unimandai.js'; 
+import Footer_Unimandai         from '../common/Footer/Footer_Unimandai.js'; 
 import HomePage                 from '../pages/HomePage/HomePage.js';
-// import HomePage_Unimandai                 from '../pages/HomePage/HomePage_Unimandai.js';
+import Unimandai_Homepage       from '../pages/HomePage/Unimandai_Homepage.js';
 
 //-----------Syatem Security Rout.................//
 
@@ -50,10 +49,6 @@ import ReturnPolicy                              from '../pages/ReturnPolicy/Ret
 import ContactPage                               from '../pages/ContactPage/ContactPage.js';
 import SiteMap                                   from '../pages/SiteMap/SiteMap.js';
 
-/*unimandai static blocks*/
-
-
-
 class Routes extends Component{
     constructor(props) {
        super(props);
@@ -65,19 +60,127 @@ class Routes extends Component{
       componentDidMount(){
         var user_ID = localStorage.getItem("user_ID");
         if (user_ID && user_ID !=='') {
-          // console.log("LogedIn");
             this.setState({
             loggedIn : true
             })
         }else{
-          // console.log("LogedOut");
             this.setState({
             loggedIn : false
             })
         }
       }
     render(){ 
-    if(this.state.loggedIn ===true){
+    if(process.env.REACT_APP_PROJECT_NAME === '4_UniMandai'){  
+    if(this.state.loggedIn ===true ){
+        const WebLayout = () => (
+          <div className="skin-blue fixed sidebar-mini">    
+            <Route path="/" exact strict component={ Unimandai_Homepage } /> 
+            <Route path="/login"                    exact strict component={ Unimandai_Homepage } />
+            <Route path="/signup"                   exact strict component={ Unimandai_Homepage } />
+            <Route path="/forgotpassword"           exact strict component={ Unimandai_Homepage } />
+            <Route path="/resetpassword/:user_ID"   exact strict component={ Unimandai_Homepage } /> 
+            <Route path="/verify-account"           exact strict component={ VerifyAccount } />
+            <Route path="/confirm-otp/:user_ID"     exact strict component={ Unimandai_Homepage } /> 
+            <Route path="/cart"                     exact strict component={ Cart } />
+            <Route path="/checkout"                 exact strict component={ Checkout } />
+            <Route path="/my-orders"                exact strict component={ MyOrders } />
+            <Route path="/view-order/:order_ID"     exact strict component={ ViewOrder } />
+            <Route path="/shipment-tracking"        exact strict component={ ShipmentTracking } />
+            <Route path="/productdetails/:productID"             exact strict component={ ProductDetailsEcommerce } />
+            <Route path="/productdetails/:productURL/:productID"             exact strict component={ ProductDetailsEcommerce } />
+            <Route path="/searchProducts"           exact strict component={ SearchProduct } />
+            <Route path="/section/:url/:sectionID"  exact strict component={ ProductCollage } />
+            <Route path="/category/:url/:sectionID/:categoryID"            exact strict component={ ProductCollage } />
+            <Route path="/subcategory/:sectionID/:categoryID/:subcategoryID"            exact strict component={ ProductCollage } />
+            <Route path="/product-collage/:categoryID/:subcategoryID"   exact strict component={ ProductCollage } />
+            
+            <Route path="/profile"              exact strict component={ Edituser } />
+            <Route path="/payment/:order_ID"            exact strict component={ Payment } />
+            <Route path="/wishlist"            exact strict component={ Wishlist } />
+            <Route path="/productreview"            exact strict component={ Productreview } />
+            <Route path="/account"            exact strict component={ Account } />
+            <Route path="/edit"            exact strict component={ EditAccount } />
+            <Route path="/address-book"            exact strict component={ AddressBook } />
+            {/* <Route path="/address/:deliveryAddressID"            exact strict component={ Address } /> */}
+            {/* <Route path="/address"            exact strict component={ Address } /> */}
+            <Route path="/returnpolicy"            exact strict component={ ReturnPolicy } />
+            <Route path="/privacypolicy"             exact strict component={PrivacyPolicy}  />
+            <Route path="/about-us"             exact strict component={Aboutuspage}  />
+            <Route path="/sitemap" exact strict component={SiteMap}  />
+            <Route path="/contact-us" exact strict component={ContactPage}  />
+          </div>
+        );
+        return (
+        <div>
+          <Router>
+            <Header_Unimandai />
+            <Switch>
+              <Route path="/" component={ WebLayout } />
+            </Switch>
+            <Footer_Unimandai />
+          </Router>
+        </div>
+        );
+    }  
+    else{
+        //const weburls = new Array('cart','checkout','my-orders','view-order','shipment-tracking','profile',);
+
+
+        const WebLayout = () => (
+          <div className="skin-blue fixed sidebar-mini">    
+            <Route path="/" exact strict component={ Unimandai_Homepage } /> 
+            <Route path="/login"                    exact strict component={ Login } />
+            <Route path="/signup"                   exact strict component={ SignUp } />
+            <Route path="/forgotpassword"           exact strict component={ ForgotPassword } />
+            <Route path="/resetpassword/:user_ID"   exact strict component={ ResetPassword } />
+            <Route path="/verify-account"           exact strict component={ VerifyAccount } />
+            <Route path="/confirm-otp/:user_ID"     exact strict component={ ConfirmOtp } />
+            <Route path="/productdetails/:productID"exact strict component={ ProductDetailsEcommerce } />
+            <Route path="/productdetails/:productURL/:productID"             exact strict component={ ProductDetailsEcommerce } />
+            <Route path="/searchProducts"           exact strict component={ SearchProduct } />
+            <Route path="/section/:url/:sectionID"  exact strict component={ ProductCollage } />
+            <Route path="/category/:url/:sectionID/:categoryID"            exact strict component={ ProductCollage } />
+            <Route path="/subcategory/:sectionID/:categoryID/:subcategoryID"            exact strict component={ ProductCollage } />
+            <Route path="/product-collage/:categoryID/:subcategoryID"   exact strict component={ ProductCollage } />
+            <Route path="/productreview"            exact strict component={ Productreview } />
+            <Route path="/returnpolicy"            exact strict component={ ReturnPolicy } />
+            <Route path="/privacypolicy"             exact strict component={PrivacyPolicy}  />
+            <Route path="/about-us"             exact strict component={Aboutuspage}  />
+            <Route path="/sitemap" exact strict component={SiteMap}  />
+            <Route path="/contact-us" exact strict component={Login}  />
+            <Route path="/cart"                     exact strict component={ Login } />
+            <Route path="/checkout"                 exact strict component={ Login } />
+            <Route path="/my-orders"                exact strict component={ Login } />
+            <Route path="/view-order/:order_ID"     exact strict component={ Login } />
+            <Route path="/shipment-tracking"        exact strict component={ Login } />
+            <Route path="/profile"              exact strict component={ Login } />
+            <Route path="/payment/:order_ID"            exact strict component={ Login } />
+            
+            <Route path="/wishlist"            exact strict component={ Login } />
+            <Route path="/account"            exact strict component={ Login } />
+            <Route path="/edit"            exact strict component={ Login } />
+            <Route path="/address-book"            exact strict component={ Login } />
+            <Route path="/address/:deliveryAddressID"            exact strict component={ Login } />
+            <Route path="/address"            exact strict component={ Login } />
+            
+          </div>
+        );
+        return (
+        <div>
+          <Router>
+            <Header_Unimandai />
+            <Switch>
+              <Route path="/" component={ WebLayout } />
+            </Switch>
+            <Footer_Unimandai />
+          </Router>
+        </div>
+        );
+    }
+   }
+   else{
+   
+       if(this.state.loggedIn ===true ){
 
         const WebLayout = () => (
           <div className="skin-blue fixed sidebar-mini">    
@@ -131,8 +234,6 @@ class Routes extends Component{
     }  
     else{
         //const weburls = new Array('cart','checkout','my-orders','view-order','shipment-tracking','profile',);
-
-
         const WebLayout = () => (
           <div className="skin-blue fixed sidebar-mini">    
             <Route path="/" exact strict component={ HomePage } /> 
@@ -142,7 +243,7 @@ class Routes extends Component{
             <Route path="/resetpassword/:user_ID"   exact strict component={ ResetPassword } />
             <Route path="/verify-account"           exact strict component={ VerifyAccount } />
             <Route path="/confirm-otp/:user_ID"     exact strict component={ ConfirmOtp } />
-            <Route path="/productdetails/:productID"             exact strict component={ ProductDetailsEcommerce } />
+            <Route path="/productdetails/:productID"exact strict component={ ProductDetailsEcommerce } />
             <Route path="/productdetails/:productURL/:productID"             exact strict component={ ProductDetailsEcommerce } />
             <Route path="/searchProducts"           exact strict component={ SearchProduct } />
             <Route path="/section/:url/:sectionID"  exact strict component={ ProductCollage } />
@@ -155,8 +256,6 @@ class Routes extends Component{
             <Route path="/about-us"             exact strict component={Aboutuspage}  />
             <Route path="/sitemap" exact strict component={SiteMap}  />
             <Route path="/contact-us" exact strict component={Login}  />
-
-
             <Route path="/cart"                     exact strict component={ Login } />
             <Route path="/checkout"                 exact strict component={ Login } />
             <Route path="/my-orders"                exact strict component={ Login } />
@@ -185,8 +284,10 @@ class Routes extends Component{
           </Router>
         </div>
         );
+      }
+   
+     }
     }
-    }
-}
+ }
 
 export default Routes;
---
+++ WebApp/onlineStore/src/coreEngine/pages/HomePage/Unimandai_Homepage.js
@@ -0,0 +1,226 @@
+import React, { Component }       from 'react';
+// import { connect }                from 'react-redux';
+// import $                          from 'jquery';
+import EcommerceProductCarousel   from "../../blocks/ProductCarouselEcommerce/EcommerceProductCarousel.js";
+import Ecommercenewproductcaro    from "../../blocks/ProductCarouselEcommerce/Ecommercenewproductcaro.js";
+import EcommerceBanner_Unimandai  from "../../blocks/Banner/EcommerceBanner_Unimandai.js";
+import ProductDivider             from "../../blocks/ProductDivider/ProductDivider.js";
+import SaleProductDivider         from "../../blocks/ProductDivider/SaleProductDivider.js"
+import WhychooseUs                from "../../blocks/WhychooseUs/WhychooseUs.js"
+import axios                      from 'axios';
+import Loader                     from "../../common/loader/Loader.js";
+import Blogs                      from "../../blocks/Blogs/Blogs.js";
+import Ceo                        from "../../blocks/CEO/Ceo.js";
+
+class HomePage extends Component {
+    constructor(props){
+    super(props);
+      this.state = {
+        featuredProducts  : [],
+        exclusiveProducts : [],
+        categories        : [],
+        exclusiveprloading:true,
+        bestsellerloading :true,
+        newproductloading :true,
+        featuredproductsloading : true
+      };
+      // this.featuredProductData();
+      // this.exclusiveProductsData();
+      // this.newProductsData();
+      // this.bestSellerData();
+    }  
+    componentDidMount() {
+      this.featuredProductData();
+      this.exclusiveProductsData();
+      this.newProductsData();
+      this.bestSellerData();
+      this.getCategories();
+      this.getWishData();
+        var refresh = window.localStorage.getItem('refresh');
+        // console.log(refresh);
+        if (refresh===null){
+            window.location.reload();
+            window.localStorage.setItem('refresh', "1");
+      }
+
+    }  
+    componentWillReceiveProps(nextProps){
+      // this.changeProductCateWise(categoryID, type);
+    }
+    featuredProductData(){
+      var productType1 = 'featured';
+      
+      axios.get("/api/products/get/listbytype/"+productType1)
+            .then((response)=>{
+              // console.log('featuredProducts' , response.data)
+              this.setState({
+                featuredproductsloading:false,
+                featuredProducts : response.data
+              })
+            })
+            .catch((error)=>{
+                // console.log('error', error);
+            })
+
+    }
+    exclusiveProductsData(){
+      var productType2 = 'exclusive';
+      axios.get("/api/products/get/listbytype/"+productType2)
+            .then((response)=>{
+
+              this.setState({
+                exclusiveprloading:false,
+                exclusiveProducts : response.data
+              })
+            })
+            .catch((error)=>{
+                // console.log('error', error);
+            })
+    }
+    newProductsData(){
+      var productType3 = 'newProduct';
+      axios.get("/api/products/get/listbytype/"+productType3)
+            .then((response)=>{
+
+              this.setState({
+                newproductloading:false,
+                newProducts : response.data
+              })
+            })
+            .catch((error)=>{
+                // console.log('error', error);
+            })    
+    }
+    bestSellerData(){
+      var productType4 = 'bestSeller';
+      axios.get("/api/products/get/listbytype/"+productType4)
+            .then((response)=>{
+              console.log("Bestseller data => ", response.data);
+              this.setState({
+                bestsellerloading  : false,
+                bestSellerProducts : response.data
+              })
+            })
+            .catch((error)=>{
+                // console.log('error', error);
+            })    
+    }
+    getCategories(){
+      axios.get("/api/category/get/list")
+      .then((response)=>{
+        this.setState({
+          categories : response.data
+        })
+      })
+      .catch((error)=>{
+        // console.log('error', error);
+      })
+    }
+    changeProductCateWise(categoryID, type){
+      axios.get("/api/products/get/listbytypeNcategory/"+categoryID+"/"+type)
+      .then((response)=>{
+        this.setState({
+          [type+"Products"] : response.data
+        },()=>{
+          this.forceUpdate();
+        })
+      })
+      .catch((error)=>{
+        // console.log('error', error);
+      })
+    }
+    getWishData(){
+      var user_ID = localStorage.getItem('user_ID');
+      axios.get('/api/wishlist/get/userwishlist/'+user_ID)
+      .then((response)=>{
+        this.featuredProductData();
+        this.exclusiveProductsData();
+        this.newProductsData();
+        this.bestSellerData();
+        this.setState({
+          wishList : response.data
+        },()=>{
+        })
+      })
+      .catch((error)=>{
+        // console.log('error', error);
+      })
+    }
+  render() {
+    var projectName=process.env.REACT_APP_PROJECT_NAME;
+    console.log("projectNmae in homepage",projectName);
+    return (
+
+      <div className="">
+        <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 backColorGray">
+          <div className="row">
+            <EcommerceBanner_Unimandai/>
+
+          </div>
+            <div className="homeRow">
+            { /*new product */}
+            {
+              this.state.exclusiveprloading ?  
+              <Loader type="carouselloader" productLoaderNo = {4}/>      
+              : 
+              (this.state.exclusiveProducts.length > 0 ? 
+                <EcommerceProductCarousel title={'FLASH SALE'} newProducts={this.state.exclusiveProducts}
+                 type={'exclusive'} categories={this.state.categories} 
+                 getWishData={this.getWishData.bind(this)} wishList={this.state.wishList}
+                 changeProductCateWise={this.changeProductCateWise.bind(this)}/>
+                :
+                null
+              )
+            }
+            {
+              this.state.bestsellerloading ?  
+              <Loader type="carouselloader" productLoaderNo = {4}/>      
+              :
+              ( this.state.bestSellerProducts.length  > 0 ? 
+                <Ecommercenewproductcaro   title={'BEST SELLERS'} newProducts={this.state.bestSellerProducts} type={'bestSeller'} getWishData={this.getWishData.bind(this)} wishList={this.state.wishList} categories={this.state.categories} changeProductCateWise={this.changeProductCateWise.bind(this)}/>
+                :
+                null
+                )
+            }
+            
+          {/*-----------------shop by category block---------------------*/}
+            <ProductDivider categories={this.state.categories} />
+            
+            {
+              this.state.newproductloading ?  
+              <Loader type="carouselloader" productLoaderNo = {4}/>      
+              :
+              (this.state.newProducts.length >0 ? 
+              <Ecommercenewproductcaro title={'NEW PRODUCTS'} newProducts={this.state.newProducts} type={'newProducts'} getWishData={this.getWishData.bind(this)} wishList={this.state.wishList} categories={this.state.categories} changeProductCateWise={this.changeProductCateWise.bind(this)}/>                
+              :
+              null )
+            }
+            
+            {
+              this.state.featuredproductsloading ?  
+              <Loader type="carouselloader" productLoaderNo = {4}/>      
+              :
+              (this.state.featuredProducts.length > 0 ? 
+                <Ecommercenewproductcaro  title={'FEATURE PRODUCTS'} newProducts={this.state.featuredProducts} type={'featured'} getWishData={this.getWishData.bind(this)} wishList={this.state.wishList} categories={this.state.categories} changeProductCateWise={this.changeProductCateWise.bind(this)}/>
+                : null
+              )
+            }
+            
+          </div>
+        </div>
+        <SaleProductDivider />
+
+         <WhychooseUs/>
+
+         <Ceo />
+
+          <Blogs />
+
+      </div>
+    );
+  }
+}
+
+
+
+export default (HomePage);
