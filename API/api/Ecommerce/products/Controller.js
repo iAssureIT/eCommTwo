e0831ad3fc81d8205d8e7ad57cb4573a1503b757
madhurighute05 <madhurighute05@gmail.com>
2020-07-04 22:51:38 +0530

    Madhuri Ghute changed product bulk upload api

--- API/api/Ecommerce/products/Controller.js
+++ API/api/Ecommerce/products/Controller.js
@@ -439,7 +439,7 @@ var insertProduct = async (section_ID, section, categoryObject, data) => {
                         _id                       : new mongoose.Types.ObjectId(),   
                         user_ID                   : vendor,  
                         vendor_ID                 : vendor, 
-                        vendorName                : vendor.split('|')[0],  
+                        vendorName                : vendor,  
                         section_ID                : section_ID,           
                         section                   : section,      
                         category                  : categoryObject.category,
